959102b10a214dbc78130114180d9083028d16a9
Randolf <zhictory@qq.com>
2020-04-07 20:25:31 +0800

    [feat] update server

--- server.1.js
+++ server.1.js
@@ -104,6 +104,7 @@ const handle = {
       } else {
         stats = fs.statSync(file);
         response.writeHead(200, {
+          "Access-Control-Allow-Origin": "*",
           "Content-Description": "File Transfer",
           "Content-Type": "application/octet-stream",
           "Content-Disposition": "attachment;filename=" + encodeURI(fileName),
